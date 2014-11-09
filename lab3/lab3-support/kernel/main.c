d5dacf0d43e30d4bef55004b8d4b4283063312ca
Omer Elhiraika <oelhiraika@gmail.com>
2014-11-08 20:42:00 -0500

    Compiling lab2 solution

--- lab3/lab3-support/kernel/main.c
+++ lab3/lab3-support/kernel/main.c
@@ -32,19 +32,19 @@ typedef enum {FALSE, TRUE} bool;
 
 /* Checks the SWI Vector Table. */
 bool check_swi_vector() {
-    int swi_vector_instr = *((int *)SWI_VECT_ADDR);
+	int swi_vector_instr = *((int *)SWI_VECT_ADDR);
 
-    // Check if the offset is negative.
-    if ((swi_vector_instr & LDR_SIGN_MASK) == 0) {
-        return FALSE;
-    }
+	// Check if the offset is negative.
+	if ((swi_vector_instr & LDR_SIGN_MASK) == 0) {
+		return FALSE;
+	}
 
-    // Check that the instruction is a (LDR pc, [pc, 0x000])
-    if ((swi_vector_instr & 0xFFFFF000) != LDR_PC_PC_INSTR) {
-        return FALSE;
-    }
+	// Check that the instruction is a (LDR pc, [pc, 0x000])
+	if ((swi_vector_instr & 0xFFFFF000) != LDR_PC_PC_INSTR) {
+		return FALSE;
+	}
 
-    return TRUE;
+	return TRUE;
 }
 
 uint32_t global_data;
@@ -52,47 +52,47 @@ uint32_t global_data;
 int kmain(int argc, char** argv, uint32_t table)
 {
 	app_startup(); /* Note that app_startup() sets all uninitialized and */ 
-			/* zero global variables to zero. Make sure to consider */
-			/* any implications on code executed before this. */
+	/* zero global variables to zero. Make sure to consider */
+	/* any implications on code executed before this. */
 	global_data = table;
 
 	/* Add your code here */
-	 if (check_swi_vector() == FALSE) {
-        return BAD_CODE;
-    }
+	if (check_swi_vector() == FALSE) {
+		return BAD_CODE;
+	}
 
-    /** Wire in the SWI handler. **/
-    // Jump offset already incorporates PC offset. Usually 0x10 or 0x14.
-    int jmp_offset = (*((int *) SWI_VECT_ADDR))&(0xFFF);
+	/** Wire in the SWI handler. **/
+	// Jump offset already incorporates PC offset. Usually 0x10 or 0x14.
+	int jmp_offset = (*((int *) SWI_VECT_ADDR))&(0xFFF);
 
-    // &S_Handler" in Jump Table.
-    int *swi_handler_addr = *(int **)(SWI_VECT_ADDR + PC_OFFSET + jmp_offset);
+	// &S_Handler" in Jump Table.
+	int *swi_handler_addr = *(int **)(SWI_VECT_ADDR + PC_OFFSET + jmp_offset);
 
-    // Save original Uboot SWI handler instructions.
-    int swi_instr_1 = *swi_handler_addr;
-    int swi_instr_2 = *(swi_handler_addr + 1);
+	// Save original Uboot SWI handler instructions.
+	int swi_instr_1 = *swi_handler_addr;
+	int swi_instr_2 = *(swi_handler_addr + 1);
 
-    // Wire in our own: LDR pc, [pc, #-4] = 0xe51ff004
-    *swi_handler_addr = 0xe51ff004;
-    *(swi_handler_addr + 1) = (int) &swi_handler; // New swi handler.
+	// Wire in our own: LDR pc, [pc, #-4] = 0xe51ff004
+	*swi_handler_addr = 0xe51ff004;
+	*(swi_handler_addr + 1) = (int) &swi_handler; // New swi handler.
 
-    // Copy argc and argv to user stack in the right order.
-    int *spTop = ((int *) USER_STACK_TOP) - 1;
-    int i = 0;
-    for (i = argc-1; i >= 0; i--) {
-        *spTop = (int)argv[i];
-        spTop--;
-    }
-    *spTop = argc;
-
-
-    /** Jump to user program. **/
-    int usr_prog_status = user_setup(spTop);
+	// Copy argc and argv to user stack in the right order.
+	int *spTop = ((int *) USER_STACK_TOP) - 1;
+	int i = 0;
+	for (i = argc-1; i >= 0; i--) {
+		*spTop = (int)argv[i];
+		spTop--;
+	}
+	*spTop = argc;
 
 
-    /** Restore SWI Handler. **/
-    *swi_handler_addr = swi_instr_1;
-    *(swi_handler_addr + 1) = swi_instr_2;
+	/** Jump to user program. **/
+	int usr_prog_status = user_setup(spTop);
+
+
+	/** Restore SWI Handler. **/
+	*swi_handler_addr = swi_instr_1;
+	*(swi_handler_addr + 1) = swi_instr_2;
 
 	return usr_prog_status;
 }
@@ -100,108 +100,106 @@ int kmain(int argc, char** argv, uint32_t table)
 
 /* Verifies that the buffer is entirely in valid memory. */
 int check_mem(char *buf, int count, unsigned start, unsigned end) {
-    unsigned start_buf = (unsigned) buf;
-    unsigned end_buf = (unsigned)(buf + count);
+	unsigned start_buf = (unsigned) buf;
+	unsigned end_buf = (unsigned)(buf + count);
 
-    if ( (start_buf < start) || (start_buf > end) ) {
-        return FALSE;
-    }
-    if ( (end_buf < start) || (end_buf > end) ) {
-        return FALSE;
-    }
-    // Overflow case.
-    if ( start_buf >= end_buf ) {
-        return FALSE;
-    }
+	if ( (start_buf < start) || (start_buf > end) ) {
+		return FALSE;
+	}
+	if ( (end_buf < start) || (end_buf > end) ) {
+		return FALSE;
+	}
+	// Overflow case.
+	if ( start_buf >= end_buf ) {
+		return FALSE;
+	}
 
-    return TRUE;
+	return TRUE;
 }
 
 // write function to replace the system's write function
 ssize_t write_handler(int fd, const void *buf, size_t count) {
 
-    // Check for invalid memory range or file descriptors
-    if (check_mem((char *) buf, (int) count, SDRAM_START, SDRAM_END) == FALSE &&
-        check_mem((char *) buf, (int) count, SFROM_START, SFROM_END) == FALSE) {
-        exit_handler(-EFAULT);
-    } else if (fd != STDOUT_FILENO) {
-        exit_handler(-EBADF);
-    }
+	// Check for invalid memory range or file descriptors
+	if (check_mem((char *) buf, (int) count, SDRAM_START, SDRAM_END) == FALSE &&
+			check_mem((char *) buf, (int) count, SFROM_START, SFROM_END) == FALSE) {
+		exit_handler(-EFAULT);
+	} else if (fd != STDOUT_FILENO) {
+		exit_handler(-EBADF);
+	}
 
-    char *buffer = (char *) buf;
-    int i;
-    char read_char;
-    for (i = 0; i < count; i++) {
-        // put character into buffer and putc
-        read_char = buffer[i];
-        putc(read_char);
-    }
-    return i;
+	char *buffer = (char *) buf;
+	size_t i;
+	char read_char;
+	for (i = 0; i < count; i++) {
+		// put character into buffer and putc
+		read_char = buffer[i];
+		putc(read_char);
+	}
+	return i;
 }
 
 
 // read function to replace the system's read function
 ssize_t read_handler(int fd, void *buf, size_t count) {
-    // Check for invalid memory range or file descriptors
-    if (check_mem((char *) buf, (int) count, SDRAM_START, SDRAM_END) == FALSE) {
-        exit_handler(-EFAULT);
-    } else if (fd != STDIN_FILENO) {
-        exit_handler(-EBADF);
-    }
+	// Check for invalid memory range or file descriptors
+	if (check_mem((char *) buf, (int) count, SDRAM_START, SDRAM_END) == FALSE) {
+		exit_handler(-EFAULT);
+	} else if (fd != STDIN_FILENO) {
+		exit_handler(-EBADF);
+	}
 
-    int i = 0;
-    char *buffer = (char *) buf;
-    char read_char;
+	size_t i = 0;
+	char *buffer = (char *) buf;
+	char read_char;
 
-    while (i < count) {
-        read_char = getc();
+	while (i < count) {
+		read_char = getc();
 
-        if (read_char == 4) { //EOT character
-            return i;
-        } else if (((read_char == 8) || (read_char == 127))) { // backspace or DEL character
-            buffer[i] = 0; // '\0' character
-            if(i > 0) {
-                i--;
-                puts("\b \b");
-            }
-        } else if ((read_char == 10) || (read_char == 13)) { // '\n' newline or '\r' carriage return character
-            buffer[i] = '\n';
-            putc('\n');
-            return (i+1);
-        } else {
-            // put character into buffer and putc
-            buffer[i] = read_char;
-            i++;
-            putc(read_char);
-        }
-    }
+		if (read_char == 4) { //EOT character
+			return i;
+		} else if (((read_char == 8) || (read_char == 127))) { // backspace or DEL character
+			buffer[i] = 0; // '\0' character
+			if(i > 0) {
+				i--;
+				puts("\b \b");
+			}
+		} else if ((read_char == 10) || (read_char == 13)) { // '\n' newline or '\r' carriage return character
+			buffer[i] = '\n';
+			putc('\n');
+			return (i+1);
+		} else {
+			// put character into buffer and putc
+			buffer[i] = read_char;
+			i++;
+			putc(read_char);
+		}
+	}
 
-    return i;
+	return i;
 }
 
 /* C_SWI_Handler uses SWI number to call the appropriate function. */
 int C_SWI_Handler(int swiNum, int *regs) {
-    int count = 0;
-    switch (swiNum) {
-        // ssize_t read(int fd, void *buf, size_t count);
-        case READ_SWI:
-            count = read_handler(regs[0], (void *) regs[1], (size_t) regs[2]);
-            break;
-        // ssize_t write(int fd, const void *buf, size_t count);
-        case WRITE_SWI:
-            count = write_handler((int) regs[0], (void *) regs[1], (size_t) regs[2]);
-            break;
-        // void exit(int status);
-        case EXIT_SWI:
-            exit_handler((int) regs[0]); // never returns
-            break;
-        default:
-            printf("Error in ref C_SWI_Handler: Invalid SWI number.");
-            exit_handler(BAD_CODE); // never returns
-    }
+	int count = 0;
+	switch (swiNum) {
+		// ssize_t read(int fd, void *buf, size_t count);
+		case READ_SWI:
+			count = read_handler(regs[0], (void *) regs[1], (size_t) regs[2]);
+			break;
+			// ssize_t write(int fd, const void *buf, size_t count);
+		case WRITE_SWI:
+			count = write_handler((int) regs[0], (void *) regs[1], (size_t) regs[2]);
+			break;
+			// void exit(int status);
+		case EXIT_SWI:
+			exit_handler((int) regs[0]); // never returns
+			break;
+		default:
+			printf("Error in ref C_SWI_Handler: Invalid SWI number.");
+			exit_handler(BAD_CODE); // never returns
+	}
 
-    return count;
+	return count;
 }
 
-	return 0;
-}
