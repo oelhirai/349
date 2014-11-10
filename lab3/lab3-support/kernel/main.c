de034572bc1a8e7c93bb3cb7058cf331f15317bf
Shashwat Srivastava <shashwat@shashwat-VirtualBox.(none)>
2014-11-10 00:47:46 -0500

    Added in irqSetup and irqHandler and modified C_SWI_Handler. Havent tested anythign yet

--- lab3/lab3-support/kernel/main.c
+++ lab3/lab3-support/kernel/main.c
@@ -14,6 +14,7 @@ typedef enum {FALSE, TRUE} bool;
 
 // 0xe59ff014 (LDR pc, [pc, 0x14]) --> 0x014 through masking
 #define SWI_VECT_ADDR 0x08
+#define IRQ_VECT_ADDR 0x18
 #define PC_OFFSET 0x08
 
 // Cannot write to this address. kernel.bin loaded here. Stack grows down.
@@ -30,9 +31,12 @@ typedef enum {FALSE, TRUE} bool;
 #define SDRAM_START 0xa0000000
 #define SDRAM_END 0xa3ffffff
 
+// Stores how much time has passed since program started
+unsigned long global_time;
+
 /* Checks the SWI Vector Table. */
-bool check_swi_vector() {
-	int swi_vector_instr = *((int *)SWI_VECT_ADDR);
+bool check_vector(int addr) {
+	int swi_vector_instr = *((int *)addr);
 
 	// Check if the offset is negative.
 	if ((swi_vector_instr & LDR_SIGN_MASK) == 0) {
@@ -49,32 +53,45 @@ bool check_swi_vector() {
 
 uint32_t global_data;
 
+int* IRQ_SWI_addr(int initAddr)
+{
+  /* Add your code here */
+  if (check_vector(initAddr) == FALSE) {
+    return BAD_CODE;
+  }
+  
+  /** Wire in the SWI handler. **/
+  // Jump offset already incorporates PC offset. Usually 0x10 or 0x14.
+  int jmp_offset = (*((int *) SWI_VECT_ADDR))&(0xFFF);
+
+  return  *(int **)(SWI_VECT_ADDR + PC_OFFSET + jmp_offset);
+}
+
 int kmain(int argc, char** argv, uint32_t table)
 {
 	app_startup(); /* Note that app_startup() sets all uninitialized and */ 
 	/* zero global variables to zero. Make sure to consider */
 	/* any implications on code executed before this. */
 	global_data = table;
-
-	/* Add your code here */
-	if (check_swi_vector() == FALSE) {
-		return BAD_CODE;
-	}
-
-	/** Wire in the SWI handler. **/
-	// Jump offset already incorporates PC offset. Usually 0x10 or 0x14.
-	int jmp_offset = (*((int *) SWI_VECT_ADDR))&(0xFFF);
+	global_time = 0;
 
 	// &S_Handler" in Jump Table.
-	int *swi_handler_addr = *(int **)(SWI_VECT_ADDR + PC_OFFSET + jmp_offset);
+	int *swi_handler_addr = IRQ_SWI_addr(SWI_VECT_ADDR);
+	int *irq_handler_addr = IRQ_SWI_addr(IRQ_VECT_ADDR);
 
 	// Save original Uboot SWI handler instructions.
 	int swi_instr_1 = *swi_handler_addr;
 	int swi_instr_2 = *(swi_handler_addr + 1);
 
+	int irq_instr_1 = *irq_handler_addr;
+	int irq_instr_2 = *(irq_handler_addr + 1);
+
 	// Wire in our own: LDR pc, [pc, #-4] = 0xe51ff004
 	*swi_handler_addr = 0xe51ff004;
 	*(swi_handler_addr + 1) = (int) &swi_handler; // New swi handler.
+
+	*irq_handler_addr = 0xe51ff004;
+	*(irq_handler_addr + 1) = (int) &irq_handler;
 
 	// Copy argc and argv to user stack in the right order.
 	int *spTop = ((int *) USER_STACK_TOP) - 1;
@@ -93,6 +110,9 @@ int kmain(int argc, char** argv, uint32_t table)
 	/** Restore SWI Handler. **/
 	*swi_handler_addr = swi_instr_1;
 	*(swi_handler_addr + 1) = swi_instr_2;
+
+	*irq_handler_addr = irq_instr_1;
+	*(irq_handler_addr + 1) = irq_instr_2;
 
 	return usr_prog_status;
 }
@@ -195,6 +215,12 @@ int C_SWI_Handler(int swiNum, int *regs) {
 		case EXIT_SWI:
 			exit_handler((int) regs[0]); // never returns
 			break;
+	        case TIME_SWI: return global_time;
+		         break;
+	case SLEEP_SWI: unsigned long timeNow = global_time;
+	                unsigned long timeSleep = (unsigned long)regs[0];
+	                while(global_time - timeNow < timeSleep);
+	                break;
 		default:
 			printf("Error in ref C_SWI_Handler: Invalid SWI number.");
 			exit_handler(BAD_CODE); // never returns
